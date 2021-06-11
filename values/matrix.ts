791b876ac1a654362d014c2cd75644e380c40496
in5net <48165199+in5net@users.noreply.github.com>
2021-06-11 14:38:20 -0400

    matrix math (+, -, *)

--- values/matrix.ts
+++ values/matrix.ts
@@ -1,13 +1,15 @@
 import { rgb24 } from 'https://deno.land/std@0.83.0/fmt/colors.ts';
 
+import Number from './number.ts';
+import Boolean from './boolean.ts';
 import Value from './value.ts';
 
 type Mat = number[][] | Float64Array[];
 
-export default class Vector extends Value {
+export default class Matrix extends Value {
   data: Float64Array[];
-  rows: number;
-  cols: number;
+  readonly rows: number;
+  readonly cols: number;
 
   constructor(data: Mat) {
     super();
@@ -29,5 +31,69 @@ export default class Vector extends Value {
     0xffff00
   )}
 ]`;
+  }
+
+  '+'(other?: Value) {
+    const { data, rows, cols } = this;
+    if (other instanceof Matrix) {
+      if (rows !== other.rows || cols !== other.cols)
+        throw 'Both matrices must be the same shape';
+      return new Matrix(
+        data.map((row, i) => row.map((x, j) => x + other.data[i][j]))
+      );
+    }
+    if (other instanceof Number)
+      return new Matrix(data.map(row => row.map(x => x + other.value)));
+    if (!other) return this;
+  }
+
+  '-'(other?: Value) {
+    const { data, rows, cols } = this;
+    if (other instanceof Matrix) {
+      if (rows !== other.rows || cols !== other.cols)
+        throw 'Both matrices must be the same shape';
+      return new Matrix(
+        data.map((row, i) => row.map((x, j) => x - other.data[i][j]))
+      );
+    }
+    if (other instanceof Number)
+      return new Matrix(data.map(row => row.map(x => x - other.value)));
+    if (!other) return new Matrix(data.map(row => row.map(x => -x)));
+  }
+
+  '*'(other: Value) {
+    const { data, rows, cols } = this;
+    if (other instanceof Matrix) {
+      if (cols !== other.rows)
+        throw 'The columns of the left bust match the rows of the right';
+
+      const ans: Mat = [];
+      for (let i = 0; i < rows; i++) {
+        ans[i] = [];
+        for (let j = 0; j < other.cols; j++) {
+          let sum = 0;
+          for (let k = 0; k < cols; k++) {
+            sum += data[i]![k]! * other.data[k]![j]!;
+          }
+          ans[i]![j] = sum;
+        }
+      }
+      return new Matrix(ans);
+    }
+    if (other instanceof Number)
+      return new Matrix(data.map(row => row.map(x => x * other.value)));
+  }
+
+  '=='(other: Value) {
+    const { data, rows, cols } = this;
+    if (other instanceof Matrix) {
+      if (rows !== other.rows || cols !== other.cols) return new Boolean(false);
+      for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+          if (data[i][j] !== other.data[i][j]) return new Boolean(false);
+        }
+      }
+      return new Boolean(true);
+    }
   }
 }
